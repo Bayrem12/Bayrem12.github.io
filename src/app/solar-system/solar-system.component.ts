6bc4ab5a124fa5862993f0b73acd12c201c996a7
Bayrem12 <bayremakka2003@gmail.com>
2024-12-01 09:53:14 +0100

    solar system change

--- src/app/solar-system/solar-system.component.ts
+++ src/app/solar-system/solar-system.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-solar-system',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './solar-system.component.html',
   styleUrl: './solar-system.component.css'
 })
