5275b0c07fb17db552ffc2def7bf9d3bb14e268d
Bayrem12 <bayremakka2003@gmail.com>
2024-12-04 19:35:26 +0100

    edit quiz

--- src/app/planet-card/planet-card.component.ts
+++ src/app/planet-card/planet-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 
+
 @Component({
   selector: 'app-planet-card',
   standalone: true,
--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,9 +1,10 @@
-import { Component, effect, inject, NgModule } from '@angular/core';
-import {  MatToolbarModule } from '@angular/material/toolbar';
+import { Component, effect, inject } from '@angular/core';
+import { MatToolbar, MatToolbarModule } from '@angular/material/toolbar';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../shared/data-access/auth.service';
-import {  MatIconModule } from '@angular/material/icon';
-import {  MatButtonModule } from '@angular/material/button';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-quiz',
@@ -12,9 +13,12 @@ import {  MatButtonModule } from '@angular/material/button';
     MatToolbarModule,
     RouterLink,
     MatIconModule,
-    MatButtonModule],
+    MatButtonModule,
+    NgIf, 
+    NgFor
+  ],
   templateUrl: './quiz.component.html',
-  styleUrl: './quiz.component.css'
+  styleUrls: ['./quiz.component.css']
 })
 export class QuizComponent {
   authService = inject(AuthService);
@@ -27,5 +31,4 @@ export class QuizComponent {
       }
     });
   }
-  
 }
